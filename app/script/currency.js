d8d1c1c1c43a59ebc89efd0a2429f0e0ca0f42e4
kasulejoseph <kasule06joseph@gmail.com>
2018-06-30 13:36:59 +0300

    first commit

---
+++ app/script/currency.js
@@ -0,0 +1,111 @@
+function loaded() {
+    let from = document.getElementById('from');
+    let to = document.getElementById('to');
+    const xHttp = new XMLHttpRequest();
+    xHttp.onreadystatechange=function(){
+        if(xHttp.readyState==4 && xHttp.status==200{
+            const obj=JSON.parse(this.responseText);
+            
+        }
+    }
+    xHttp.open('GET', 'https://free.currencyconverterapi.com/api/v5/currencies', true)
+    xHttp.send()
+}
+
+
+/////////////////////////////////////
+
+if (!('indexedDB' in window)) {
+    console.log('This browser doesn\'t support IndexedDB');
+}
+
+var dbPromise = idb.open('currency-converter', 1, function(upgradeDb) {
+  switch (upgradeDb.oldVesrion) {
+    case 0:
+    upgradeDb.createObjectStore('currency', { keyPath: 'id' });
+  }
+});
+    
+
+fetch('https://free.currencyconverterapi.com/api/v5/currencies')
+  .then(
+    response => {
+      if (response.status !== 200) {
+        console.log(`Looks like there was a problem. Status Code: ${response.status}`);
+        return;
+      }
+      response.json().then(results => {
+        let currencySelect;
+            let i;
+            currencySelect = document.querySelectorAll('.currencyChange');
+        
+            for (i = 0; i < currencySelect.length; i++) {
+            currencySelect[i].length = 0;
+            let defaultCurrency = document.createElement('option');
+            defaultCurrency.text = 'Select a Currency';
+            currencySelect[i].add(defaultCurrency);
+            currencySelect[i].selectedIndex = 0;
+        let option;
+        for (const result in results) {
+            for (const id in results[result]) {
+            
+            let data = results[result][id]['id'];
+          
+            option = document.createElement('option');
+            option.text = data;
+                 currencySelect[i].add(option)
+           }
+          }
+        }
+
+      });
+    }
+  )
+  .catch(err => {
+    console.log('Fetch Error :-S', err);
+  });
+
+document.getElementById('convert').onclick = calculateAmt;
+
+//////////////////////////////////////////////
+
+function calculateAmt() {
+
+    let frmCurrency = document.getElementById('currencyFrm').value;
+    
+    let toCurrency = document.getElementById('currencyTo').value;
+    
+    let query = `${frmCurrency}_${toCurrency}`;
+    
+    amt = document.getElementById('amount').value;
+
+    fetch(`https://free.currencyconverterapi.com/api/v5/convert?q=${query}&compact=ultra`)
+  .then(
+    response => {
+      if (response.status !== 200) {
+        console.log(`Looks like there was a problem. Status Code: ${response.status}`);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(data => {
+        console.log(data);
+
+        for (query in data) {
+                if (data.hasOwnProperty(query)) {
+                    let exchangeRate = data[query];
+                    let total = amt * exchangeRate;
+                    let currConv = Math.round(total * 100) / 100;
+                    console.log(currConv);
+                    document.getElementById('output').value = currConv;
+                }
+              }
+      });
+    }
+  )
+  .catch(err =>  {
+    console.log('Fetch Error :-S', err);
+  });
+}
+
+/////////////////////////////////////
---
+++ app/sw.js
@@ -0,0 +1,26 @@
+var staticCacheName = 'currency-converter';
+
+self.addEventListener('install', function(event) {
+ event.waitUntil(
+   caches.open(staticCacheName).then(function(cache) {
+     return cache.addAll([
+       'index.html',
+       'curr.css',
+   	   'calc.js',
+       'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css',
+       'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css',
+       'https://free.currencyconverterapi.com/api/v5/countries',
+       'https://marcodredd.github.io/CurrencyConverter/'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(async function() {
+		const cachedResponse = await caches.match(event.request);
+		if (cachedResponse) return cachedResponse;
+		return fetch(event.request);
+	}());
+});
+	
